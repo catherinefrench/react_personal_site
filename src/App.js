91228737cbc51ad348d06278b953e03e82765beb
Catherine French <catherine.french@me.com>
2019-12-02 09:59:57 -0500

    Adds styling, commiting before converting to scss

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ function App() {
       <header className="App-header">
         <div class="menu-container"> 
           <ul>
-            <li>Home</li>
-            <li>Work</li>
-            <li>Goals</li>
-            <li>Hobbies</li>
-            <li>Sports</li>
-            <li>Analytics</li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Work</a></li>
+            <li><a href="#">Goals</a></li>
+            <li><a href="#">Hobbies</a></li>
+            <li><a href="#">Sports</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Contact</a></li>
           </ul>
         </div>
       </header>
@@ -29,8 +30,7 @@ function App() {
                 <div class="section">
                         <div class="first tile">
                             <div class="text work">
-                              <p>I have 3 years experience working in Web Support for Q4 Inc, a Toronto-based fintech and global leader in capital markets solutions. I led a team of 9 people using HTML5, CSS3, and JavaScript on a daily basis for 850+ clients including Facebook, Apple, Oracle and Intel.</p>
-                              <p>Although when entering this role I was new to web development, with my strong work ethic within 6 months I was the most productive team member. I was promoted the fastest out of 18 team members for my ability to learn quickly, create solid processes that helped us scale, and for my leadership mindset. In my next role I’d like to gain more exposure with React, a library I have taught myself this year.</p>
+                              <p>I'm currently looking for a job as a front-end developer. I have 3 years experience working in Web Support for Q4 Inc, a Toronto-based fintech and global leader in capital markets solutions. I led a team of 9 people using HTML5, CSS3, and JavaScript on a daily basis for 850+ clients including Facebook, Apple, Oracle and Intel.</p>
                             </div>
                             <div class="photo">
                                 <img src={require('./collaboration.jpg')} alt=""/>
@@ -39,7 +39,10 @@ function App() {
                     </div>
             <div class="section">
                 <div class="second tile">
-                    <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+                    <div class="text">
+                        <p>Although when entering this role I was new to web development, with my strong work ethic within 6 months I was the most productive team member. I was promoted the fastest out of 18 team members for my ability to learn quickly, create solid processes that helped us scale, and for my leadership mindset. In my next role I’d like to gain more exposure with React, a library I have taught myself this year.</p>
+                    </div>
+                    
                     <div class="photo">
                       <img src={require('./collaboration.jpg')} alt=""/>
                     </div>
