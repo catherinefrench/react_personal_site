2d9b83b3672268bbbf9a517166b76877014c91de
Catherine French <catherine.french@me.com>
2019-12-03 23:11:53 -0500

    Fixes background svg border issue

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Work.scss';
+import collaboration from '../collaboration.jpg'
 
 const Work = () => {
 
@@ -8,10 +9,10 @@ const Work = () => {
         <div className="title">Work</div>
         <div className="first tile"> 
             <div className="text work">
-              <p>I'm currently looking for a job as a front-end developer. I have 3 years experience working in Web Support for Q4 Inc, a Toronto-based fintech and global leader in capital markets solutions. I led a team of 9 people using HTML5, CSS3, and JavaScript on a daily basis for 850+ clients including Facebook, Apple, Oracle and Intel.</p>
+              <p>I'm currently looking for a role as a front-end developer. I have 3 years experience working in Web Support for Q4 Inc, a Toronto-based fintech and global leader in capital markets solutions. I led a team of 9 people using HTML5, CSS3, and JavaScript on a daily basis for 850+ clients including Facebook, Apple, Oracle and Intel.</p>
             </div>
             <div className="photo">
-                {/* <img src={require('./collaboration.jpg')} alt=""/> */}
+                <img src={require('../collaboration.jpg')} alt=""/>
             </div>
         </div>
   </div>
