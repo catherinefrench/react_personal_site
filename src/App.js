2802d10f2675fa05f21c2e1391b99686ff96259a
Catherine French <catherine.french@me.com>
2019-12-06 16:42:55 -0500

    Bug fix for extra active div now removed from below app container

--- src/App.js
+++ src/App.js
@@ -19,22 +19,12 @@ class App extends Component {
         <header className="app-header">
         <div className="menu-container"> 
           <ul>
-
               <li><AnchorLink href="#home" offset='50'><Link to="/">Home</Link></AnchorLink></li> 
               <li><AnchorLink href="#work" offset='50'><Link to="/work">Work</Link></AnchorLink></li>
               <li><AnchorLink href="#stack" offset='50'><Link to="/stack">Stack</Link></AnchorLink></li> 
               <li><AnchorLink href="#experience" offset='50'><Link to="/experience">Experience</Link></AnchorLink></li> 
               <li><AnchorLink href="#connect" offset='50'><Link to="/connect">Connect</Link></AnchorLink></li> 
-
-
           </ul>
-          {/* <ul>
-            <li><AnchorLink href="#home" offset='50'><Link to={'/'}>Home</Link></AnchorLink></li> 
-            <li><AnchorLink href="#work" offset='50'><Link to={'/work'} offset='50'>Work</Link></AnchorLink></li>
-            <li><AnchorLink href="#stack" offset='50'><Link to={'/stack'} offset='50'>Stack</Link></AnchorLink></li> 
-            <li><AnchorLink href="#experience" offset='50'><Link to={'/experience'} offset='50'>Experience</Link></AnchorLink></li> 
-            <li><AnchorLink href="#connect" offset='50'><Link to={'/connect'} offset='50'>Connect</Link></AnchorLink></li> 
-          </ul> */}
         </div>
       </header>
         <Banner />
@@ -46,16 +36,17 @@ class App extends Component {
             <Connect className="section" />
           </div>
         </div>
+        <Switch>
+              <Route exact path='/'> </Route> 
+              <Route path='/work'></Route>   
+              <Route path='/stack'></Route>   
+              <Route path='/experience'></Route> 
+              <Route path='/connect'></Route> 
+        </Switch>
         <Footer />
 
       </div>
-        <Switch>
-              <Route exact path='/'> </Route> 
-              <Route path='/work'><Work /></Route>   
-              <Route path='/stack'><Stack /></Route>   
-              <Route path='/experience'> <Experience /></Route> 
-              <Route path='/connect'> <Connect /></Route> 
-        </Switch>
+
     </Router>
 
     );
