a940b454f0db700ea5e489150b5e13f8726ff1bc
Catherine French <catherine.french@me.com>
2019-12-06 15:34:47 -0500

    Adds routes to project using BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.scss';
-import Navigation from './Navigation/Navigation';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+// import Navigation from './Navigation/Navigation';
 import Banner from './Banner/Banner';
 import Work from './Work/Work';
 import Stack from './Stack/Stack';
@@ -8,23 +10,45 @@ import Experience from './Experience/Experience';
 import Connect from './Connect/Connect';
 import Footer from './Footer/Footer';
 
-function App() {
-  return (
-    <div className="App" id="home">
-      <Navigation />
-      <Banner />
-      <div className="content-container">
-
-        <div className="content-bottom">
-          <Work />
-          <Stack />
-          <Experience />
-          <Connect className="section" />
+class App extends Component {
+  render() {
+    return (
+    <Router>
+      <div className="App" id="home">
+        {/* <Navigation /> */}
+        <header className="app-header">
+        <div className="menu-container"> 
+          <ul>
+            <li><AnchorLink href="#home" offset='50'><Link to={'/'}>Home</Link></AnchorLink></li> 
+            <li><AnchorLink href="#work" offset='50'><Link to={'/work'} offset='50'>Work</Link></AnchorLink></li>
+            <li><AnchorLink href="#stack" offset='50'><Link to={'/stack'} offset='50'>Stack</Link></AnchorLink></li> 
+            <li><AnchorLink href="#experience" offset='50'><Link to={'/experience'} offset='50'>Experience</Link></AnchorLink></li> 
+            <li><AnchorLink href="#connect" offset='50'><Link to={'/connect'} offset='50'>Connect</Link></AnchorLink></li> 
+          </ul>
         </div>
+      </header>
+        <Banner />
+        <div className="content-container">
+
+          <div className="content-bottom">
+            <Work />
+            <Stack />
+            <Experience />
+            <Connect className="section" />
+          </div>
+        </div>
+        <Footer />
+        <Switch>
+              <Route exact path='/' component={Work} />
+              <Route path='/stack' component={Stack} />
+              <Route path='/experience' component={Experience} />
+              <Route path='/connect' component={Connect} />
+        </Switch>
       </div>
-      <Footer />
-    </div>
-  );
+    </Router>
+
+    );
+  };
 }
 
 export default App;
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,22 +1,27 @@
-import React from 'react'; 
-import './Navigation.scss';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+// import React from 'react'; 
+// import './Navigation.scss';
+// import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 
-const Navigation = () => {
-    return (
-        <header className="app-header">
-        <div className="menu-container"> 
-          <ul>
-            <li><AnchorLink href="#home">Home</AnchorLink></li> 
-            <li><AnchorLink href="#work" offset='50'>Work</AnchorLink></li>
-            <li><AnchorLink href="#stack" offset='50'>Stack</AnchorLink></li> 
-            <li><AnchorLink href="#experience" offset='50'>Experience</AnchorLink></li> 
-            <li><AnchorLink href="#connect" offset='50'>Connect</AnchorLink></li> 
-          </ul>
-        </div>
-      </header>
-    )
-}; 
+// const Navigation = () => {
+//     return (
+//         <header className="app-header">
+//         <div className="menu-container"> 
+//           <ul>
+//             <li><Link to={'/'}>Home</Link></li> 
+//             <li><Link to={'/work'} offset='50'>Work</Link></li>
+//             <li><Link to={'/stack'} offset='50'>Stack</Link></li> 
+//             <li><Link to={'/experience'} offset='50'>Experience</Link></li> 
+//             <li><Link to={'/connect'} offset='50'>Connect</Link></li> 
+            
+//             {/* <li><AnchorLink href="#work" offset='50'>Work</AnchorLink></li>
+//             <li><AnchorLink href="#stack" offset='50'>Stack</AnchorLink></li> 
+//             <li><AnchorLink href="#experience" offset='50'>Experience</AnchorLink></li> 
+//             <li><AnchorLink href="#connect" offset='50'>Connect</AnchorLink></li>  */}
+//           </ul>
+//         </div>
+//       </header>
+//     )
+// }; 
 
-export default Navigation;
+// export default Navigation;
